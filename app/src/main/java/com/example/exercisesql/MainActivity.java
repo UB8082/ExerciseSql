93a1a95e3b4d5d24b4c564e078d7444b7d55750b
Lucky Ahmad Ardiansyah <lucky.ahmad.ft18@mail.umy.ac.id>
2020-04-20 22:02:36 +0700

    test

---
+++ app/src/main/java/com/example/exercisesql/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.exercisesql;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fab = new Intent(MainActivity.this, DisplayMhs.class);
+                //saat pindah, activity yg sekarang langsung ditutup
+                //agar saat menekan tombol kembali tidak bolak-balik
+                finish();
+            }
+        });
+    }
+
+
+
+
+
+}
