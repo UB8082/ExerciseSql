8e72fc75496e9127f349bba00688d367dff6fa29
Lucky Ahmad Ardiansyah <lucky.ahmad.ft18@mail.umy.ac.id>
2020-04-21 11:07:02 +0700

    test2

--- app/src/main/java/com/example/exercisesql/DisplayMhs.java
+++ app/src/main/java/com/example/exercisesql/DisplayMhs.java
@@ -3,12 +3,22 @@ package com.example.exercisesql;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.widget.EditText;
 
 public class DisplayMhs extends AppCompatActivity {
-
+EditText nama;
+EditText phone;
+EditText Alamat;
+EditText Email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_mhs);
+        nama = (EditText) findViewById(R.id.editTextNama);
+        phone = (EditText) findViewById(R.id.editTextName);
+        Email = (EditText) findViewById(R.id.editTextEmail);
+        Alamat = (EditText) findViewById(R.id.editTextPhone);
+
     }
 }
